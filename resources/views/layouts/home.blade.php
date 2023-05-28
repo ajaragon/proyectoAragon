50a5c28c134afd5e3772fd092d513429148b6921
ajaragon <julianaragongithub@outlook.es>
2023-05-29 00:14:59 +0200

    6.3V

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -49,55 +49,49 @@
     </head>
     <body>
         <!--https://codepen.io/codervishwas/pen/OJmGyWQ-->
-        <header class="header">
-            <!-- Barra de navegación -->
-            <nav class="navbar
-                        color-navbar
-                        background-navbar
-                        bg-dark
-                        navbar-expand-md
-                        text-uppercase">
+        <!--https://www.youtube.com/watch?v=_MrShB9fh7U-->
+        <header class="header color-fondo-hf color-letras-hf">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <div class="container-fluid">
 
-                <div class="container">
-                    
-                        <!-- Nombre de la empresa y el logo -->
-                        <a class="navbar-brand" href="#">
-                        <img    src="{{ url('images/logo.png') }}"
-                                alt="logo"
-                                class="img-logo mr-2"
-                                id="logo">Empresa
-                        </a>
-                        
-                        <!-- Donde se oculta el menú en pantallas más pequeñas -->
-                        <button     data-bs-toggle="collapse" 
-                                    data-bs-target="header-menu"
-                                    type="button" 
-                                    class="navbar-toggler">
-                            <span class="navbar-toggler-icon"></span>
+                        <a class="navbar-brand" href="#"><img src="{{ url('images/logo.png') }}" id="logo"></a>
+                        <h5 class="d-none d-lg-block">Empresa</h5>
+
+                        <!-- Botón hamburguesa -->
+                        <button class="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="toggler-icon top-bar"></span>
+                            <span class="toggler-icon middle-bar"></span>
+                            <span class="toggler-icon bottom-bar"></span>
                         </button>
-                                
-                        <!-- Páginas del sitio web -->
-                        <div class="collapse navbar-collapse" id="header-menu">
-                            <ul class="navbar-nav ms-auto">
+
+                        <!-- Menú -->
+                        <div class="collapse navbar-collapse ms-lg-5" id="navbarNav">
+                            <ul class="navbar-nav">
                                 <li class="nav-item">
-                                    <a class="active nav-link" href="#">Home</a>
+                                    <a class="nav-link active text-reset" aria-current="page" href="#">Home</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Quiénes somos</a>
+                                    <a class="nav-link active text-reset" aria-current="page" href="#">Quiénes somos</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Blog</a>
+                                    <a class="nav-link active text-reset" aria-current="page" href="#">Blog</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Contáctanos</a>
+                                    <a class="nav-link active text-reset" aria-current="page" href="#">Contacto</a>
                                 </li>
                             </ul>
                         </div>
+                        <!-- Fin de menú -->
 
-                        <!-- Cambiar idioma -->
-
-                </div>
-            </nav>
+                        <!-- Login/Register -->
+                        <div class="col-auto">
+                            <a href="#" class="text-reset pr-2">Login</a>
+                            <a href="#" class="text-reset">Register</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
             <script src="https://kit.fontawesome.com/a076d05399.js"></script>
         </header>
         <main>
@@ -113,51 +107,110 @@
                             .Bovino
                             .Vacuno
                         -iframe de la localización
-                        -Certificados
-                        -Imágenes del negocio
                     -->
                         <!-- Logo + nombre de la empresa -->
                         <!-- Navigation -->
                     <!--https://codepen.io/taviskaron01/pen/MWrrLzZ-->
+                    <!--https://mataderodehuesca.com/#block-block-4-->
+                    <!--https://www.youtube.com/watch?v=CZ_fUrs3tL4&list=PLUW3XAK9O3HHv6LPCxTDLvg_YQMVNeiJZ&index=3-->
+            
+            <!-- SERVICIOS -->
+            <section class="text-reset text-center p-4">
+                <h4>EMPRESA</h4>
+                <p>Empresa familiar dedicada a la ganadería desde 1990</p>
+                <div class="row">
 
+                    <!-- Recepción -->
+                    <article class="col-4 card-effect">
+                        <div class="card">
+                            <img    src="{{ url('images/galeria-rebaño.jpg') }}"
+                                    alt="rebaño"
+                                    class="card-img card-img-filter imagen-galeria">
+                                    
+                        </div>
+                        <div class="">
+                            <h6 class="mt-3">Recepción</h6>
+                            <p>No encargamos de la recepción de los animales</p>
+                        </div>
+                    </article>
+
+                    <!-- Sacrificio -->
+                    <article class="col-4 card-effect">
+                        <div class="card">
+                            <img    src="{{ url('images/galeria-corrales-2.jpg') }}"
+                                    alt="rebaño"
+                                    class="card-img card-img-filter imagen-galeria">
+                        </div>
+                        <div class="">
+                            <h6 class="mt-3">Sacrificio</h6>
+                            <p>No encargamos de los sacrificios de los animales</p>
+                        </div>
+                    </article>
+
+                    <!-- Almacenamiento -->
+                    <article class="col-4 card-effect">
+                        <div class="card">
+                            <img    src="{{ url('images/galeria-matadero-1.jpg') }}"
+                                    alt="rebaño"
+                                    class="card-img card-img-filter imagen-galeria">
+                        </div>
+                        <div class="">
+                            <h6 class="mt-3">Almacenamiento</h6>
+                            <p>No encargamos del almacenamiento de los animales</p>
+                        </div>
+                    </article>
+                </div>
+            </section>
+            <section class="iframe align-content-center">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1205.5241693150017!2d-3.7067995896263244!3d40.41482319885855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smadrid%20plaza%20mayor!5e0!3m2!1ses!2ses!4v1685311921344!5m2!1ses!2ses" 
+                        width="600" 
+                        height="450" 
+                        style="border:0;" 
+                        allowfullscreen="" 
+                        loading="lazy" 
+                        referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+            </section>
         </main>
         <!--https://www.youtube.com/watch?v=KIEZqYZhcEU-->
         <!--https://www.youtube.com/watch?v=PSD5pFi6bx4-->
         <!--https://www.youtube.com/watch?v=CM_iZHTEZ3s-->
-        <footer class="footer bg-dark text-white py-4">
+        <footer class="footer color-fondo-hf color-letras-hf text-white py-4">
             <div class="container">
-                <nav class="row">
+                <nav class="navbar navbar-expand-lg navbar-light justify-content-around">
+                    <div class="container-fluid">
 
-                    <!-- Logo -->
-                    <a href="#" class="col-4 d-flex align-items-center">
-                        <img    src="{{ url('images/logo.png') }}"
-                                alt="logo"
-                                class="img-logo mr-2"
-                                id="logo">
-                    </a>
+                        <!-- Logo -->
+                        <a href="#" class="col-lg-4 d-flex align-items-center">
+                            <img    src="{{ url('images/logo.png') }}"
+                                    alt="logo"
+                                    class="img-logo mr-2"
+                                    id="logo">
+                        </a>
 
-                    <!-- Dirección de la empresa -->
-                    <ul class="col-4 list-unstyled">
-                        <li>Dirección Plaza Mayor</li>
-                        <li>28012, Madrid, España</li>
-                        <li>Distrito Sol</li>
-                    </ul>
+                        <!-- Dirección de la empresa -->
+                        <ul class="col-lg-4 col-sm-12 list-unstyled">
+                            <li class="text-reset">Dirección Plaza Mayor</li>
+                            <li class="text-reset">28012, Madrid, España</li>
+                            <li class="text-reset">Distrito Sol</li>
+                        </ul>
 
-                    <!-- Redes sociales -->
-                    <ul class="col-4 list-unstyled">
-                        <li class="font-weight-bold text-uppercase">Nuestras redes sociales:</li>
-                        <li class="d-flex justify-content-between">
-                            <a href="#">
-                                <i class="fab fa-twitter" id="twitter"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fab fa-facebook-f" id="facebook"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fab fa-instagram" id="instagram"></i>
-                            </a>
-                        </li>
-                    </ul>
+                        <!-- Redes sociales -->
+                        <ul class="col-lg-4 list-unstyled">
+                            <li class="font-weight-bold text-uppercase">Nuestras redes sociales:</li>
+                            <li class="d-flex justify-content-between">
+                                <a href="#" class="text-reset">
+                                    <i class="fab fa-twitter" id="twitter"></i>
+                                </a>
+                                <a href="#" class="text-reset">
+                                    <i class="fab fa-facebook-f" id="facebook"></i>
+                                </a>
+                                <a href="#" class="text-reset">
+                                    <i class="fab fa-instagram" id="instagram"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </footer>
